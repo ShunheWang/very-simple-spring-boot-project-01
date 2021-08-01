834069c482f2de6d7e15ffa4362e230d1f48082c
ShunheWang <maisonwangsh@gmail.com>
2021-08-01 15:41:46 +0800

    Learn spring-boot

--- src/main/java/com/maison/onYoutube/student/StudentControl.java
+++ src/main/java/com/maison/onYoutube/student/StudentControl.java
@@ -42,7 +42,6 @@ public class StudentControl {
             @PathVariable("studentId") Long studentId,
             @RequestParam(required = false) String name,
             @RequestParam(required = false) String email) {
-
         studentService.updateStudent(studentId, name, email);
     }
 
